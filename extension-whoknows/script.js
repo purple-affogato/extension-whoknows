5d1f9e55e76b34a69165cdccd7595e88f4155d53
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-07-26 13:13:26 -0400

    fixed display settings so that window is a fixed shape

--- extension-whoknows/script.js
+++ extension-whoknows/script.js
@@ -20,7 +20,9 @@ var list = [
     "And I walk like a lion, I'm making my own history. I don't know the answers, I just go my own way.",
     "I'm a confident person, but I do get depressed and cry alone at home sometimes. You can't help that because we're humans. It's okay to cry and make mistakes. People always make mistkaes and learn from them. Fall down again and again, then get up one more time. That's a confident woman.",
     "Be on the side that saves people. If both sides are the same, then choose to become a good person. Save the weak, protect the orphaned. You might not see a great difference between right and wrong, but... saving others is something just a bit wonderful.",
-    "live, laugh, hehe :)"
+    "live, laugh, hehe :)",
+    "Ah, inspiration, it comes, it comes.",
+    "A song can go on even without ornaments or extra direction, as long as it has the main melody to be played. That's the most important thing. That's you. You're the main thing, the important thing, the conductor of my orchaestra. I love you. I love you like that."
 ];
 
 document.getElementById("button1").addEventListener("click", getQuote)
@@ -28,5 +30,4 @@ document.getElementById("button1").addEventListener("click", getQuote)
 function getQuote() {
     var rand = Math.floor(Math.random() * list.length);
     document.getElementById("quote").innerHTML = list[rand];
-    console.log("Generated random quote");
 }
